6509219b6caa0e5c211acedbaf07bb81c27377e8
bruno <bru.costa.bc@gmail.com>
2019-11-11 09:44:15 -0500

    Fewer input arguments

--- examples/run_preprocessing.sh
+++ examples/run_preprocessing.sh
@@ -3,11 +3,8 @@
 # Script to run preprocessing on WiFi dataset.
 
 python ../preprocessing/main.py \
-    --task 1Cv2 \
     --train_tsv /scratch/RFMLS/RFML_Test_Specs_Delivered_v3/test1/1Cv2.train.tsv \
     --test_tsv /scratch/RFMLS/RFML_Test_Specs_Delivered_v3/test1/1Cv2.test.tsv \
     --root_wifi /mnt/nas/bruno/MILCOM/data/wifi_sigmf_dataset_gfi_1/ \
     --out_root_data /mnt/nas/bruno/MILCOM/data/v3/ \
     --out_root_list /mnt/nas/bruno/MILCOM/data/v3_list/ \
-    --wifi_eq \
-    --time_analysis
--- examples/train_model.sh
+++ examples/train_model.sh
@@ -3,39 +3,11 @@
 # Script to build, train, and test a model on a given dataset.
 
 python -u ../train_val_framework/run_framework.py \
-    --exp_name test_run \
-    --base_path /scratch/RFMLS/dec18_darpa/v3_list/raw_samples/1Cv2/wifi/ \
-    --stats_path /scratch/RFMLS/dec18_darpa/v3_list/raw_samples/1Cv2/wifi/ \
+    --pickle_files /mnt/nas/bruno/MILCOM/data/v3_list/1Cv2/wifi/raw_samples/ \
     --save_path /home/bruno/MILCOM/ \
-    --save_predictions \
-    --task 1Cv2 \
-    --data_type wifi \
-    --file_type mat \
-    --val_from_train \
     --model_flag baseline \
-    --slice_size 198 \
-    --devices 50 \
     --cnn_stack 5 \
     --fc_stack 2 \
-    --channels 128 \
-    --fc1 256 \
-    --fc2 128 \
-    --add_padding \
-    --K 16 \
-    --training_strategy big \
     --epochs 25 \
-    --batch_size 512 \
-    --lr 0.0001 \
     --id_gpu $1 \
-    --early_stopping \
-    --normalize \
-    --patience 1 \
     --train \
-    --test_stride 16 \
-    --flag_error_analysis \
-    --confusion_matrix \
-    --get_device_acc 5 \
-    --time_analysis \
-    --visualize_training \
-    #> /home/bruno/MILCOM/log.out \
-    #2> /home/bruno/MILCOM/log.err 
